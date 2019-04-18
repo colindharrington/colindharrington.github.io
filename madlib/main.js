145812b5bf05c9632c2a2726db3a55f394452f92
colindharrington <harrc386@newschool.edu>
2019-04-17 21:10:33 -0400

    up2

--- madlib/main.js
+++ madlib/main.js
@@ -1,18 +1,25 @@
 $(function() {
-	console.log('hello');
-
 
 $.get('https://colindharrington.github.io/api.json', function(data) {
   console.log(data);
 
   $('.adj1').html(data.adjective1);
+  $('.adj2').html(data.adjective2);
+  $('.art').html(data.artSupply);
 
 
 
-}).fail(function() {
+}).fail(function(error) {
   console.log('Error in GET request. Handle the error gracefully.');
+  console.error
 });
 
 
+$('.button').click(function() {
+	console.log($(this));
+	var url = $(this.data('url');
+	console.log(url)
+
+});
 
 });
